319aba9cca89acc878f34045c3339aad3967e021
watsonpeter <watsonpeter65@gmail.com>
2022-02-15 16:21:29 +1100

    second commit

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -41,8 +41,7 @@ urlpatterns = [
     path('', home_view),             #    index   root
     path('articles/', article_search_view),
     path('articles/create/', article_create_view),
- #   path('articles/create/', article_create_view),
- #   path('articles/<int:id>/', article_detail_view),    
+    path('articles/<int:id>/', article_detail_view),    
     path('admin/', admin.site.urls),
     path('login/', login_view) ,    
     path('logout/', logout_view) ,    
--- trydjango/views.py
+++ trydjango/views.py
@@ -1,31 +1,30 @@
 """
-to render HTML Files
+To render html web pages
 """
 import random
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 from articles.models import Article
 
-
-#   routied to here  by /  from urls.py
 def home_view(request, *args, **kwargs):
-    """       take in a request  return an HTML reponse     """
-    print('why are we in homer view   z z z z z z')
-       
+    """
+    Take in a request (Django sends request)
+    Return HTML as a response (We pick to return the response)
+    """
+    name = "Justin" # hard coded
+    random_id = random.randint(1, 4) # pseudo random
     
+    # from the database??
     article_obj = Article.objects.all().first()
     article_queryset = Article.objects.all()
     context = {
         "object_list": article_queryset,
         "object": article_obj,
-       }
+    }
     # Django Templates
-    HTML_STRING = render_to_string("home-view.html",context = context)
-    # HTML_STRING = """  
-    # <h1>{title} (id : {id}) ! </h1> 
-    # <p> {content}  </p>
-    # """ .format(**content)
+    HTML_STRING = render_to_string("home-view.html", context=context)
+    # HTML_STRING = """
+    # <h1>{title} (id: {id})!</h1>
+    # <p>{content}!</p>
+    # """.format(**context)
     return HttpResponse(HTML_STRING)
-
-
-
